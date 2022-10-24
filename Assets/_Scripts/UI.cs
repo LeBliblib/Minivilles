407f0d8448eee847441283cd6e684491cfdf67b0
Yuna <yuna.bonnifet@gmail.com>
2022-10-25 00:46:45 +0200

    Sound when zooming on cards

--- Assets/_Scripts/UI.cs
+++ Assets/_Scripts/UI.cs
@@ -7,6 +7,11 @@ public class UI : MonoBehaviour
     //Vector3 startPos;
     Vector3 normalscale;
 
+    public AudioSource flip;
+    public AudioClip clip;
+
+    private bool isPlaying = false;
+
     void Start()
     {
         //startPos = transform.position;
@@ -14,6 +19,13 @@ public class UI : MonoBehaviour
     }
     public void OnMouseOver()
     {
+        if (isPlaying == false) //pour éviter les échos
+        {
+            flip.PlayOneShot(clip, 0.5f); 
+            isPlaying = true;
+        }
+        
+
         //transform.position = new Vector3(0, 0, 0);
         transform.localScale = new Vector3(0.7f, 0.7f, 0);
         GetComponent<SpriteRenderer>().sortingOrder = 5;
@@ -21,6 +33,7 @@ public class UI : MonoBehaviour
 
     public void OnMouseExit()
     {
+        isPlaying = false;
         //transform.position = startPos;
         transform.localScale = normalscale;
         GetComponent<SpriteRenderer>().sortingOrder = 0;
