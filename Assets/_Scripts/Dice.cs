dbf3d426ef68989ce490963548df7cffe056c022
Kaïros <101337357+KairosGames@users.noreply.github.com>
2022-11-02 09:45:39 +0100

    dice
    dice

--- Assets/_Scripts/Dice.cs
+++ Assets/_Scripts/Dice.cs
@@ -15,39 +15,10 @@ public class Dice : MonoBehaviour
     public GameObject diceEnd;
     public  GameObject animator;
 
-    //private Vector3 startPos = new Vector3(-10, 0, 0);
-    //private Vector3 endPos = new Vector3(2, 0, 0);
-
-    /*
-
-   public int Roll()
-   {
-        RollTheDice();
-        return finalSide;
-   }
-    private void Awake()
-    {
-        Instance = this;
-    }
-    private void Start()
-    {
-        rend = GetComponent<SpriteRenderer>();
-    }
-    public void RollTheDice()
-    {
-        int randomDiceSide = 0;
-        finalSide = 0;
-        for (int i = 0; i <= 20; i++)
-        {
-            randomDiceSide = Random.Range(0, 6);
-            Debug.Log(diceSides.Length);
-            rend.sprite = diceSides[randomDiceSide];
-        }
-        finalSide = randomDiceSide + 1;
-        Debug.Log(finalSide);
-    }
-}
-    */
+    Vector3 startPosition;
+    Vector3 rollSpeed;
+    Vector3 startDiceScale;
+    Vector3 endDiceScale;
 
 
     public int Roll()
@@ -67,6 +38,10 @@ public class Dice : MonoBehaviour
    private void Awake()// a supp ?
     {
         Instance = this;
+        startPosition = new Vector3(-5.0f, 0, 0);
+        rollSpeed = new Vector3(5.0f, 0, 0);
+        startDiceScale = new Vector3(2.0f, 2.0f, 1.0f);
+        endDiceScale = new Vector3(1.0f, 1.0f, 1.0f);
     }
 
     private void Start()
@@ -79,6 +54,9 @@ public class Dice : MonoBehaviour
 
     public IEnumerator RollTheDice()
     {
+        transform.position = startPosition;
+        transform.localScale = startDiceScale;
+        LeanTween.scale(gameObject, endDiceScale, 1.1f).setEase(LeanTweenType.easeOutBounce);
         move = true;
         animator.SetActive(true);
         diceEnd.SetActive(false);
@@ -87,16 +65,14 @@ public class Dice : MonoBehaviour
 
         animator.SetActive(false);
         diceEnd.SetActive(true);
-        ///move = false;
+        move = false;
     }
 
     public void Update()
     {
         if (move)
-            //transform.position = startPos;
-            //transform.position = Vector3.MoveTowards(startPos, endPos, 0.5f * Time.deltaTime);
+            transform.position += rollSpeed * Time.deltaTime;
 
-            transform.position = Vector3.left * Time.deltaTime * 3; // Marche pas, semble bloqué sur la scène.
 
     }
 }
